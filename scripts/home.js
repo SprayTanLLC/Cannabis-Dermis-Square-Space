69f86de724c3c77567d0d332bb009c21a60b7848
spraytanllc <spraytanllc@gmail.com>
2020-10-25 14:49:23 -0500

    added new test page

--- scripts/home.js
+++ scripts/home.js
@@ -1,47 +1,26 @@
-var sun = new Image();
-var moon = new Image();
-var earth = new Image();
-function init() {
-    sun.src = 'https://mdn.mozillademos.org/files/1456/Canvas_sun.png';
-    moon.src = 'https://mdn.mozillademos.org/files/1443/Canvas_moon.png';
-    earth.src = 'https://mdn.mozillademos.org/files/1429/Canvas_earth.png';
-    window.requestAnimationFrame(draw);
-}
 
-function draw() {
-    var ctx = document.getElementById('canvas').getContext('2d');
+/**
+ * This script wrapped in a Immediately-Invoked Function Expression (IIFE) to
+ * prevent variables from leaking onto the global scope. For more information
+ * on IIFE visit the link below.
+ * @see http://en.wikipedia.org/wiki/Immediately-invoked_function_expression
+ */
 
-    ctx.globalCompositeOperation = 'destination-over';
-    ctx.clearRect(0, 0, 300, 300); // clear canvas
+(function () {
+    'use strict';
 
-    ctx.fillStyle = 'rgba(0, 0, 0, 0.4)';
-    ctx.strokeStyle = 'rgba(0, 153, 255, 0.4)';
-    ctx.save();
-    ctx.translate(150, 150);
+    // Load all images via Squarespace's Responsive ImageLoader
+    function loadAllImages() {
+        var images = document.querySelectorAll('img[data-src]');
+        for (var i = 0; i < images.length; i++) {
+            ImageLoader.load(images[i], { load: true });
+        }
+    }
 
-    // Earth
-    var time = new Date();
-    ctx.rotate(((2 * Math.PI) / 60) * time.getSeconds() + ((2 * Math.PI) / 60000) * time.getMilliseconds());
-    ctx.translate(105, 0);
-    ctx.fillRect(0, -12, 50, 24); // Shadow
-    ctx.drawImage(earth, -12, -12);
+    // The event subscription that loads images when the page is ready
+    document.addEventListener('DOMContentLoaded', loadAllImages);
 
-    // Moon
-    ctx.save();
-    ctx.rotate(((2 * Math.PI) / 6) * time.getSeconds() + ((2 * Math.PI) / 6000) * time.getMilliseconds());
-    ctx.translate(0, 28.5);
-    ctx.drawImage(moon, -3.5, -3.5);
-    ctx.restore();
+    // The event subscription that reloads images on resize
+    window.addEventListener('resize', loadAllImages);
 
-    ctx.restore();
-
-    ctx.beginPath();
-    ctx.arc(150, 150, 105, 0, Math.PI * 2, false); // Earth orbit
-    ctx.stroke();
-
-    ctx.drawImage(sun, 0, 0, 300, 300);
-
-    window.requestAnimationFrame(draw);
-}
-
-init();
+}());
